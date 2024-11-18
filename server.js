8923c4cce5189057f3c654c49ec33932cd9fc321
saananokela <smnokela@gmail.com>
2024-11-18 15:56:30 +0200

    Added root-endpoint

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app = express();
 
 const port = process.env.PORT  || 3001;
 
+// root-endpoint
+app.get('/', (req, res) => {
+  res.send('Welcome to the Movie API');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
