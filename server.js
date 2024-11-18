15dfc50b4f79c935b182a66a87b67fbada9a9f7d
saananokela <smnokela@gmail.com>
2024-11-18 16:16:47 +0200

    Added RAW endpoint for adding movies genre

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Movie API');
 });
 
+// add movie genre -endpoint
+app.post('/genres', (req, res) => {
+  res.send('Genre added successfully');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
