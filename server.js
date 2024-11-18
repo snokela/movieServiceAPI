07526771d8754fc60f9cfbe1fabfe7fb08ed091b
saananokela <smnokela@gmail.com>
2024-11-18 16:03:22 +0200

    Added JSON and urlencoded middlewares

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ import express from 'express';
 var app = express();
 
 const port = process.env.PORT  || 3001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // root-endpoint
 app.get('/', (req, res) => {
   res.send('Welcome to the Movie API');
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
